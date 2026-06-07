304b34c457a7ce289f0b58dcc1f7b569825f88cd
Balthild <ibalthild@gmail.com>
2026-06-07 14:17:26 -0500

    refactor: split file

---
+++ Sources/PliersServer/PliersServer+Configure.swift
@@ -0,0 +1,50 @@
+import Fluent
+import FluentSQLiteDriver
+import JWT
+import PliersCommon
+import Vapor
+
+extension PliersServer {
+	func configure() async throws {
+		try await database()
+		try await console()
+		try await http()
+
+		// jwt is used for short-lived temporary login tokens only,
+		// so the key can be randomly generated on each launch
+		let key = SymmetricKey(size: .bits256)
+		await app.jwt.keys.add(hmac: .init(key: key), digestAlgorithm: .sha256)
+	}
+
+	private func console() async throws {
+		app.asyncCommands.commands.removeValue(forKey: "routes")
+		app.asyncCommands.commands.removeValue(forKey: "migrate")
+	}
+
+	private func database() async throws {
+		let file = config.state.appending(path: "db.sqlite")
+		let config = DatabaseConfigurationFactory.sqlite(.file(file.path))
+		app.databases.use(config, as: .sqlite)
+
+		app.migrations.add(SessionRecord.migration)
+		app.migrations.add(CreateUser())
+		try await app.autoMigrate()
+	}
+
+	private func http() async throws {
+		app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+		app.sessions.configuration.cookieName = "pliers_session"
+		app.sessions.use(.fluent)
+		app.middleware.use(app.sessions.middleware)
+
+		try app.register(collection: HomeController())
+
+		try app.register(collection: AuthController())
+		try app.register(collection: TokenLoginController())
+		try app.register(collection: PasskeyLoginController())
+		try app.register(collection: PasswordLoginController())
+
+		try app.register(collection: DashboardController())
+	}
+}
--- Sources/PliersServer/PliersServer.swift
+++ Sources/PliersServer/PliersServer.swift
@@ -9,8 +9,8 @@ import PliersCommon
 import Vapor
 
 public struct PliersServer {
-	private let config: Config
-	private let app: Application
+	let config: Config
+	let app: Application
 
 	public static func make(_ config: Config) async throws -> Self {
 		var env = Environment.production
@@ -71,50 +71,5 @@ public struct PliersServer {
 
 		try await app.servers.asyncCommand.run(using: &context)
 		try await app.running?.onStop.get()
-	}
-}
-
-extension PliersServer {
-	private func configure() async throws {
-		try await database()
-		try await console()
-		try await http()
-
-		// jwt is used for short-lived temporary login tokens only,
-		// so the key can be randomly generated on each launch
-		let key = SymmetricKey(size: .bits256)
-		await app.jwt.keys.add(hmac: .init(key: key), digestAlgorithm: .sha256)
-	}
-
-	private func console() async throws {
-		app.asyncCommands.commands.removeValue(forKey: "routes")
-		app.asyncCommands.commands.removeValue(forKey: "migrate")
-	}
-
-	private func database() async throws {
-		let file = config.state.appending(path: "db.sqlite")
-		let config = DatabaseConfigurationFactory.sqlite(.file(file.path))
-		app.databases.use(config, as: .sqlite)
-
-		app.migrations.add(SessionRecord.migration)
-		app.migrations.add(CreateUser())
-		try await app.autoMigrate()
-	}
-
-	private func http() async throws {
-		app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-		app.sessions.configuration.cookieName = "pliers_session"
-		app.sessions.use(.fluent)
-		app.middleware.use(app.sessions.middleware)
-
-		try app.register(collection: HomeController())
-
-		try app.register(collection: AuthController())
-		try app.register(collection: TokenLoginController())
-		try app.register(collection: PasskeyLoginController())
-		try app.register(collection: PasswordLoginController())
-
-		try app.register(collection: DashboardController())
 	}
 }
