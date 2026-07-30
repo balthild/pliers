2d2a90d1db80ef9f589cf96d0092374b8c19ccd6
Balthild <ibalthild@gmail.com>
2026-07-30 06:25:23 -0500

    feat: support zstd tarball

--- Sources/PliersServer/View/Page/File/FileListPage.swift
+++ Sources/PliersServer/View/Page/File/FileListPage.swift
@@ -384,7 +384,7 @@ extension View.Page {
 									if !entry.dir {
 										a(.href(link(to: entry.path, action: "download"))) { "Download" }
 
-										let pattern = /.*\.(\.zip|tar|tar\.gz|tgz|tar\.bz2|tbz2|tar\.xz|txz)$/
+										let pattern = /.*\.(zip|tar|t(ar\.)?(gz|bz2|xz|zst))$/
 										if path.wholeMatch(of: pattern) != nil {
 											button(
 												.class("link"),
